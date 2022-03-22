81d13ef2be0c008ff9cb0ec57cba5e024ab8d6fa
Yanting Chi <davechi@sjtu.edu.cn>
2022-03-22 11:56:03 +0800

    first commit

---
+++ tigress/3.0/test/test1.c
@@ -0,0 +1,14 @@
+#include "tigress.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int x=0;
+    if(x>1&&x<5){
+        printf("in");
+    }
+
+    return 0;
+}
---
+++ tigress/3.0/test/test1_trans.c
@@ -0,0 +1,14 @@
+#include "tigress.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int x=1;
+if (((unsigned int )((((x - 1) & - (x >= 1)) >> 31) - (((x - 1) & - (x >= 1)) ^ (((x - 1) & - (x >= 1)) >> 31))) >> 31U) & 1) {
+    if (((unsigned int )(((((5 - x) & - (5 >= x)) + ((5 - x) & - (5 >= x))) & (((5 - x) & - (5 >= x)) >> 31)) - ((5 - x) & - (5 >= x))) >> 31U) & 1) {
+      printf((char const   */* __restrict  */)"in");
+    }
+  }
+  return (0);}
